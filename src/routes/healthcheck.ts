a09849b77ada24b7a04f0010c6254bf06a4b4be5
Tegar Aditya <tegaraditya53@gmail.com>
2025-05-23 19:48:52 +0700

    feat: add healthcheck route for API status verification

---
+++ src/routes/healthcheck.ts
@@ -0,0 +1,14 @@
+import { Hono } from "hono"
+
+const healthcheck = new Hono()
+
+healthcheck.get("/", async (c) => {
+  try {
+      return c.json({ message: "OK" }, 200)
+  } catch (error) {
+    console.error(error)
+    return c.json({ message: error }, 500)
+  }
+})
+
+export default healthcheck
